933c526c99a8c0b9db667ef7a66b25afd4b6cb3e
anjalibhoge <anjalibhoge@gmail.com>
2020-09-28 22:36:22 +0530

    Initial commit

---
+++ JavaBasicProject/src/com/demo/MainClass.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee("Harsh",23);
+		System.out.println(employee);
+	}
+
+}
